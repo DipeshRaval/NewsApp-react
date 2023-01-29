7a0280880eab73461344658141160b4b50969b8d
Dipeshrvl <dipeshraval0007@gmail.com>
2023-01-29 01:51:57 -0800

    Updates

---
+++ precache-manifest.c600c94d6e6c56b387dc808136993673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aaad9cf01f01aaeeeaa",
+    "url": "/NewsApp-react/static/css/main.e4794f73.chunk.css"
+  },
+  {
+    "revision": "1aaad9cf01f01aaeeeaa",
+    "url": "/NewsApp-react/static/js/main.1aaad9cf.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/NewsApp-react/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "03f584b0a76b74716507",
+    "url": "/NewsApp-react/static/js/2.03f584b0.chunk.js"
+  },
+  {
+    "revision": "7aa0f06e9a899661ddf6",
+    "url": "/NewsApp-react/static/js/runtime~main.7aa0f06e.js"
+  },
+  {
+    "revision": "cc71f02e1606f7a1424cca4eeeac5570",
+    "url": "/NewsApp-react/static/media/load.cc71f02e.gif"
+  },
+  {
+    "revision": "938c499f24937ab4fb93b04a229bb1ab",
+    "url": "/NewsApp-react/index.html"
+  }
+];
